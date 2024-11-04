38b01d7485838c7d8c830ec623fb2813c1d3a14c
vorsptr <voros.peti58@gmail.com>
2024-11-04 01:34:27 +0100

    Player created on the screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print(f'''
@@ -8,15 +9,23 @@ def main():
           Screen height: {SCREEN_HEIGHT}
           ''')
     pygame.init()
+    fpsClock = pygame.time.Clock()
+    dt = 0
+    
     pygame.display.set_caption('Asteroids')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    fpsClock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            screen.fill('black')
-            pygame.display.flip()
+        screen.fill('black')
+        player.draw(screen)
+        pygame.display.flip()
+        time = fpsClock.tick(60)
+        dt = time / 1000
+        print(dt)
+        
     
 if __name__ == '__main__':
     main()
